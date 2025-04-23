25b89172478245fa6a5e2c23f7ba85ab6bca09bd
Dmitriy Safronov <zimniy@cyberbrain.pw>
2025-04-23 19:05:04 +0400

    feature: Department
    Signed-off-by: Dmitriy Safronov <zimniy@cyberbrain.pw>

--- src/myapp1/admin.py
+++ src/myapp1/admin.py
@@ -1,11 +1,29 @@
 from django.contrib import admin
 
-from myapp1.models import Worker
+from myapp1.models import Department, Worker
 
-# Register your models here.
+
+class DepartmentAdmin(admin.ModelAdmin):
+    def get_fields_custom():
+        # return [field.name for field in Department._meta.get_fields()]
+        return ["title", "phone"]
+
+    list_display = get_fields_custom()
+    search_fields = get_fields_custom()
+    list_filter = get_fields_custom()
+
+
+admin.site.register(Department, DepartmentAdmin)
+
+
 class WorkerAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Worker._meta.get_fields()]
-    search_fields = [field.name for field in Worker._meta.get_fields()]
-    list_filter = [field.name for field in Worker._meta.get_fields()]
+    def get_fields_custom():
+        # return [field.name for field in Worker._meta.get_fields()]
+        return ["name", "second_name", "salary", "department"]
+
+    list_display = get_fields_custom()
+    search_fields = get_fields_custom()
+    list_filter = get_fields_custom()
+
 
 admin.site.register(Worker, WorkerAdmin)
--- src/myapp1/models.py
+++ src/myapp1/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    phone = models.CharField(max_length=20, blank=False)
+
 class Worker(models.Model):
     name = models.CharField(max_length=20, blank=False)
     second_name = models.CharField(max_length=35, blank=False)
     salary = models.IntegerField(default=0)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
