7356f97b35b7b079c53a7cd66af98a605ff5cfca
Dmitriy Safronov <zimniy@cyberbrain.pw>
2025-04-23 19:37:36 +0400

    refactor: format
    Signed-off-by: Dmitriy Safronov <zimniy@cyberbrain.pw>

--- src/myapp1/models.py
+++ src/myapp1/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
 class Department(models.Model):
     title = models.CharField(max_length=100, blank=False)
     phone = models.CharField(max_length=20, blank=False)
+
 
 class Worker(models.Model):
     name = models.CharField(max_length=20, blank=False)
