723b1787b846a6bc8b79c2786942272c95544cc5
Dmitriy Safronov <zimniy@cyberbrain.pw>
2025-04-23 19:52:36 +0400

    refactor: вернулся к примеру деда-программиста
    Signed-off-by: Dmitriy Safronov <zimniy@cyberbrain.pw>

--- src/myapp1/admin.py
+++ src/myapp1/admin.py
@@ -1,29 +1,6 @@
 from django.contrib import admin
-
 from myapp1.models import Department, Worker
 
+admin.site.register(Department)
 
-class DepartmentAdmin(admin.ModelAdmin):
-    def get_fields_custom():
-        # return [field.name for field in Department._meta.get_fields()]
-        return ["title", "phone"]
-
-    list_display = get_fields_custom()
-    search_fields = get_fields_custom()
-    list_filter = get_fields_custom()
-
-
-admin.site.register(Department, DepartmentAdmin)
-
-
-class WorkerAdmin(admin.ModelAdmin):
-    def get_fields_custom():
-        # return [field.name for field in Worker._meta.get_fields()]
-        return ["name", "second_name", "salary", "department"]
-
-    list_display = get_fields_custom()
-    search_fields = get_fields_custom()
-    list_filter = get_fields_custom()
-
-
-admin.site.register(Worker, WorkerAdmin)
+admin.site.register(Worker)
--- src/myapp1/models.py
+++ src/myapp1/models.py
@@ -5,9 +5,15 @@ class Department(models.Model):
     title = models.CharField(max_length=100, blank=False)
     phone = models.CharField(max_length=20, blank=False)
 
+    def __str__(self):
+        return f"{self.title} {self.phone}"
+
 
 class Worker(models.Model):
     name = models.CharField(max_length=20, blank=False)
     second_name = models.CharField(max_length=35, blank=False)
     salary = models.IntegerField(default=0)
     department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.second_name} {self.name}"
