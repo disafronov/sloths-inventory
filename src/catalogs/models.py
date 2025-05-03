30e0e684f2924f1a6b36edc634576339e37b5441
Dmitrii Safronov <zimniy@cyberbrain.pw>
2025-05-04 00:07:09 +0400

    feat: добавить модель Type и обновить модель Device для использования нового поля
    Добавлена новая модель Type с соответствующими полями и настройками. Обновлены модель Device и админка для включения нового поля type, что улучшает структуру данных и функциональность админки.
    
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.pw>

--- src/catalogs/models.py
+++ src/catalogs/models.py
@@ -42,9 +42,24 @@ class Model(models.Model):
     def __str__(self):
         return self.name
 
+class Type(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Название")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    class Meta:
+        verbose_name = "Тип"
+        verbose_name_plural = "Типы"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 class Device(models.Model):
     catalog_number = models.CharField(max_length=255, unique=True, verbose_name="Номенклатурный номер")
     category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категория")
+    type = models.ForeignKey(Type, on_delete=models.PROTECT, verbose_name="Тип")
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.PROTECT, verbose_name="Производитель")
     model = models.ForeignKey(Model, on_delete=models.PROTECT, verbose_name="Модель")
     description = models.TextField(blank=True, verbose_name="Описание")
@@ -54,8 +69,8 @@ class Device(models.Model):
     class Meta:
         verbose_name = "Устройство"
         verbose_name_plural = "Устройства"
-        ordering = ['catalog_number', 'category', 'manufacturer', 'model']
-        unique_together = ['category', 'manufacturer', 'model']
+        ordering = ['catalog_number', 'category', 'type', 'manufacturer', 'model']
+        unique_together = ['category', 'type', 'manufacturer', 'model']
 
     def __str__(self):
-        return f"{self.catalog_number} {self.category} {self.manufacturer} {self.model}"
+        return f"{self.catalog_number} {self.category} {self.type} {self.manufacturer} {self.model}"
