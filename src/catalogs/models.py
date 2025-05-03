ca4aa9e9b939bf29941471c9124e5f98eb471a50
Dmitrii Safronov <zimniy@cyberbrain.pw>
2025-05-03 22:48:11 +0400

    feat: изменить поле serial_number на catalog_number в модели Device и обновить админку для отображения и поиска по новому полю
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.pw>

--- src/catalogs/models.py
+++ src/catalogs/models.py
@@ -46,7 +46,7 @@ class Device(models.Model):
     category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name="Категория")
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, verbose_name="Поставщик")
     model = models.ForeignKey(Model, on_delete=models.CASCADE, verbose_name="Модель")
-    serial_number = models.CharField(max_length=255, unique=True, verbose_name="Серийный номер")
+    catalog_number = models.CharField(max_length=255, unique=True, verbose_name="Номенклатурный номер")
     description = models.TextField(blank=True, verbose_name="Описание")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
@@ -54,7 +54,7 @@ class Device(models.Model):
     class Meta:
         verbose_name = "Устройство"
         verbose_name_plural = "Устройства"
-        ordering = ['category', 'vendor', 'model', 'serial_number']
+        ordering = ['category', 'vendor', 'model', 'catalog_number']
 
     def __str__(self):
-        return f"{self.category} {self.vendor} {self.model} {self.serial_number}"
+        return f"{self.category} {self.vendor} {self.model} {self.catalog_number}"
