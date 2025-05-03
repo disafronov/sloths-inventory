71aee59aaa3797b80409d1ece70d9b5de533a0cc
Dmitrii Safronov <zimniy@cyberbrain.pw>
2025-05-03 22:35:48 +0400

    feat: изменить порядок отображения и фильтрации полей в админке для моделей Vendor, Model и Device
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.pw>

--- src/catalogs/admin.py
+++ src/catalogs/admin.py
@@ -3,21 +3,21 @@ from .models import Vendor, Model, Device
 
 @admin.register(Vendor)
 class VendorAdmin(admin.ModelAdmin):
-    list_display = ("name", "description", "created_at", "updated_at")
+    list_display = ("name", "description", "updated_at", "created_at")
     list_display_links = ("name", "description")
     search_fields = ("name", "description")
-    list_filter = ("created_at", "updated_at")
+    list_filter = ("updated_at", "created_at")
 
 @admin.register(Model)
 class ModelAdmin(admin.ModelAdmin):
-    list_display = ("name", "description", "created_at", "updated_at")
+    list_display = ("name", "description", "updated_at", "created_at")
     list_display_links = ("name", "description")
     search_fields = ("name", "description")
-    list_filter = ("created_at", "updated_at")
+    list_filter = ("updated_at", "created_at")
 
 @admin.register(Device)
 class DeviceAdmin(admin.ModelAdmin):
-    list_display = ("vendor", "model", "serial_number", "description", "created_at", "updated_at")
+    list_display = ("vendor", "model", "serial_number", "description", "updated_at", "created_at")
     list_display_links = ("vendor", "model", "serial_number", "description")
     search_fields = ("serial_number", "description")
-    list_filter = ("vendor", "model", "created_at", "updated_at")
+    list_filter = ("vendor", "model", "updated_at", "created_at")
