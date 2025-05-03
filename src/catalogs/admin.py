b33cf2cda78b30be84af9f12b4bd0f1dee6f875e
Dmitrii Safronov <zimniy@cyberbrain.pw>
2025-05-03 22:21:38 +0400

    feat: улучшить админку Django, добавив настройки для моделей Vendor, Model и Device
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.pw>

--- src/catalogs/admin.py
+++ src/catalogs/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import Vendor, Model, Device
 
-admin.site.register(Vendor)
-admin.site.register(Model)
-admin.site.register(Device)
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "created_at", "updated_at")
+    search_fields = ("name", "description")
+    list_filter = ("created_at", "updated_at")
+
+@admin.register(Model)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "created_at", "updated_at")
+    search_fields = ("name", "description")
+    list_filter = ("created_at", "updated_at")
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ("vendor", "model", "serial_number", "created_at", "updated_at")
+    search_fields = ("serial_number", "description")
+    list_filter = ("vendor", "model", "created_at", "updated_at")
