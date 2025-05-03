43e3192fdfb3c2720b27177eea3300933cead2c8
Dmitrii Safronov <zimniy@cyberbrain.pw>
2025-05-03 22:43:37 +0400

    feat: добавить модель Category и обновить админку для отображения и фильтрации устройств по категориям
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.pw>

--- src/catalogs/models.py
+++ src/catalogs/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Название")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 class Vendor(models.Model):
     name = models.CharField(max_length=255, unique=True, verbose_name="Название")
     description = models.TextField(blank=True, verbose_name="Описание")
@@ -29,6 +43,7 @@ class Model(models.Model):
         return self.name
 
 class Device(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name="Категория")
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, verbose_name="Поставщик")
     model = models.ForeignKey(Model, on_delete=models.CASCADE, verbose_name="Модель")
     serial_number = models.CharField(max_length=255, unique=True, verbose_name="Серийный номер")
@@ -39,7 +54,7 @@ class Device(models.Model):
     class Meta:
         verbose_name = "Устройство"
         verbose_name_plural = "Устройства"
-        ordering = ['vendor', 'model', 'serial_number']
+        ordering = ['category', 'vendor', 'model', 'serial_number']
 
     def __str__(self):
-        return f"{self.vendor} {self.model} {self.serial_number}"
+        return f"{self.category} {self.vendor} {self.model} {self.serial_number}"
