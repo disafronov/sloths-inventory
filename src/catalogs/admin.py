d0bf4c6498a265dd38449a00d70e01d7fe01ef48
Dmitrii Safronov <zimniy@cyberbrain.pw>
2025-05-03 22:43:37 +0400

    feat: добавить модель Category и обновить админку для отображения и фильтрации устройств по категориям
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.pw>

--- src/catalogs/admin.py
+++ src/catalogs/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Vendor, Model, Device
+from .models import Category, Vendor, Model, Device
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "updated_at", "created_at")
+    list_display_links = ("name", "description")
+    search_fields = ("name", "description")
+    list_filter = ("updated_at", "created_at")
 
 @admin.register(Vendor)
 class VendorAdmin(admin.ModelAdmin):
@@ -17,7 +24,7 @@ class ModelAdmin(admin.ModelAdmin):
 
 @admin.register(Device)
 class DeviceAdmin(admin.ModelAdmin):
-    list_display = ("vendor", "model", "serial_number", "description", "updated_at", "created_at")
-    list_display_links = ("vendor", "model", "serial_number", "description")
+    list_display = ("category", "vendor", "model", "serial_number", "description", "updated_at", "created_at")
+    list_display_links = ("category", "vendor", "model", "serial_number", "description")
     search_fields = ("serial_number", "description")
-    list_filter = ("vendor", "model", "updated_at", "created_at")
+    list_filter = ("category", "vendor", "model", "updated_at", "created_at")
