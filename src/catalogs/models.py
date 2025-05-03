185b34f5f09bc56ba6d9b42cd03982a018f85204
Dmitrii Safronov <zimniy@cyberbrain.pw>
2025-05-03 22:06:35 +0400

    feat: добавить модель Vendor и зарегистрировать её в админке Django
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.pw>

--- src/catalogs/models.py
+++ src/catalogs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Vendor(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Название")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+
+    class Meta:
+        verbose_name = "Поставщик"
+        verbose_name_plural = "Поставщики"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
