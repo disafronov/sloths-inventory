f8e6045f6bfb442dbe9716bda48248e383db526f
Dmitrii Safronov <zimniy@cyberbrain.pw>
2025-05-04 20:07:57 +0400

    feat: добавить модели Category, Manufacturer, Model и Type, а также админку для управления ими
    Созданы новые модели Category, Manufacturer, Model и Type для хранения информации о категориях, производителях, моделях и типах устройств. Также добавлена админка для управления этими моделями с необходимыми полями и улучшенным интерфейсом. Это улучшает структуру данных и управление устройствами.
    
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.pw>

---
+++ src/devices/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Название")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Название")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    class Meta:
+        verbose_name = "Производитель"
+        verbose_name_plural = "Производители"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Model(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Название")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    class Meta:
+        verbose_name = "Модель"
+        verbose_name_plural = "Модели"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Type(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Название")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    class Meta:
+        verbose_name = "Тип"
+        verbose_name_plural = "Типы"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
