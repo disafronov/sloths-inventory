dc0873cb79008a5cb2dbbb33a53c8d7b385e7910
Dmitrii Safronov <zimniy@cyberbrain.pw>
2025-05-04 20:07:57 +0400

    feat: добавить модели Category, Manufacturer, Model и Type, а также админку для управления ими
    Созданы новые модели Category, Manufacturer, Model и Type для хранения информации о категориях, производителях, моделях и типах устройств. Также добавлена админка для управления этими моделями с необходимыми полями и улучшенным интерфейсом. Это улучшает структуру данных и управление устройствами.
    
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.pw>

---
+++ src/devices/admin.py
@@ -0,0 +1,54 @@
+from django.contrib import admin
+from .models import Category, Manufacturer, Model, Type
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "updated_at", "created_at")
+    list_display_links = ("name", "description")
+    search_fields = ("name", "description")
+    list_filter = ("updated_at", "created_at")
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description', 'updated_at', 'created_at')
+        }),
+    )
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "updated_at", "created_at")
+    list_display_links = ("name", "description")
+    search_fields = ("name", "description")
+    list_filter = ("updated_at", "created_at")
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description', 'updated_at', 'created_at')
+        }),
+    )
+
+@admin.register(Model)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "updated_at", "created_at")
+    list_display_links = ("name", "description")
+    search_fields = ("name", "description")
+    list_filter = ("updated_at", "created_at")
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description', 'updated_at', 'created_at')
+        }),
+    )
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "updated_at", "created_at")
+    list_display_links = ("name", "description")
+    search_fields = ("name", "description")
+    list_filter = ("updated_at", "created_at")
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description', 'updated_at', 'created_at')
+        }),
+    )
