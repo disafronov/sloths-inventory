684fd1495849da60495a4b0e78015d87366b7ac2
Dmitrii Safronov <zimniy@cyberbrain.pw>
2025-05-18 19:05:35 +0400

    feat: добавление функционала аутентификации с кастомными представлениями входа и выхода
    Созданы маршруты для входа и выхода, добавлены соответствующие представления и шаблоны. Обновлены настройки для перенаправления пользователей после аутентификации. Добавлены тесты для проверки функционала аутентификации.
    
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.pw>

--- src/common/urls.py
+++ src/common/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .health import liveness, readiness
+from .views import CustomLoginView, CustomLogoutView
+
+app_name = 'common'
 
 urlpatterns = [
     path('liveness/', liveness, name='liveness'),
     path('readiness/', readiness, name='readiness'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ] 
