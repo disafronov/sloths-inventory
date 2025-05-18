911a8576738eb4f94452b65098d66abc930c41e9
Dmitrii Safronov <zimniy@cyberbrain.pw>
2025-05-19 02:57:37 +0400

    fix: исправление пути для статических файлов и удаление дублирующего CSS
    Обновлен путь для статических файлов в настройках Django с добавлением начального слэша. Удалена дублирующая ссылка на CSS файл 'select2.min.css' в шаблоне base.html, что улучшает структуру и предотвращает загрузку лишних ресурсов.
    
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.pw>

--- src/sloths_inventory/settings.py
+++ src/sloths_inventory/settings.py
@@ -152,11 +152,13 @@ LANGUAGE_COOKIE_PATH = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
