38c02327eac051ec6769ca185237f47b2f692033
Dmitrii Safronov <zimniy@cyberbrain.pw>
2025-05-19 00:38:18 +0400

    feat: обновление файлов локализации для поддержки интернационализации
    Обновлены файлы локализации для английского и русского языков в приложениях catalogs и devices. Изменены даты создания POT-файлов, добавлены переводы для новых полей моделей, а также исправлены пути к моделям. Это улучшает доступность приложения для пользователей с разными языковыми предпочтениями.
    
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.pw>

--- src/devices/models.py
+++ src/devices/models.py
@@ -1,21 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from common.models import BaseModel
 from .attributes import Category, Manufacturer, Model, Type
 
 
 class Device(BaseModel):
     category = models.ForeignKey(
-        Category, on_delete=models.PROTECT, verbose_name="Категория"
+        Category, on_delete=models.PROTECT, verbose_name=_("Category")
     )
-    type = models.ForeignKey(Type, on_delete=models.PROTECT, verbose_name="Тип")
+    type = models.ForeignKey(Type, on_delete=models.PROTECT, verbose_name=_("Type"))
     manufacturer = models.ForeignKey(
-        Manufacturer, on_delete=models.PROTECT, verbose_name="Производитель"
+        Manufacturer, on_delete=models.PROTECT, verbose_name=_("Manufacturer")
     )
-    model = models.ForeignKey(Model, on_delete=models.PROTECT, verbose_name="Модель")
+    model = models.ForeignKey(Model, on_delete=models.PROTECT, verbose_name=_("Model"))
 
     class Meta:
-        verbose_name = "Устройство"
-        verbose_name_plural = "Устройства"
+        verbose_name = _("Device")
+        verbose_name_plural = _("Devices")
         ordering = ["category", "type", "manufacturer", "model"]
         unique_together = ["category", "type", "manufacturer", "model"]
 
