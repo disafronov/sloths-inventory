9a849e4e79660c50581f8e3284882fd45ffbeeba
Dmitrii Safronov <zimniy@cyberbrain.pw>
2025-05-19 01:55:32 +0400

    feat: обновление поддержки интернационализации в приложениях catalogs, devices и inventory
    Изменены verbose_name для приложений catalogs, devices и inventory с использованием gettext для поддержки интернационализации. Добавлены переводы для английского и русского языков в соответствующие файлы локализации, что улучшает доступность приложения для пользователей с разными языковыми предпочтениями.
    
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.pw>

--- src/devices/apps.py
+++ src/devices/apps.py
@@ -1,9 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 import os
 
 
 class DevicesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "devices"
-    verbose_name = "Устройства"
+    verbose_name = _("Devices")
     path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "devices")
