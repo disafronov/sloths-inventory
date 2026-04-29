a00b9ec1883e1e8d7762ee7888da2b01cf2f0c33
Dmitrii Safronov <zimniy@cyberbrain.cc>
2026-04-29 20:25:49 +0400

    refactor: remove home view and associated template
    - Deleted the HomeView class and its corresponding template, as the main user-facing entry point has been moved to the inventory UI.
    - Updated URL patterns to reflect the removal of the home route.
    - Cleaned up localization files by removing references to the home template.
    
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.cc>

--- src/common/views.py
+++ src/common/views.py
@@ -1,14 +1,6 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import redirect
-from django.views.generic import TemplateView
+"""
+Common views for the project.
 
-
-class HomeView(TemplateView):
-    template_name = "home.html"
-
-    def get(
-        self, request: HttpRequest, *args: object, **kwargs: object
-    ) -> HttpResponse:
-        if request.user.is_authenticated:
-            return redirect("inventory:my-items")
-        return super().get(request, *args, **kwargs)
+This module is intentionally small: the main user-facing entrypoint lives in
+the inventory UI (root route).
+"""
