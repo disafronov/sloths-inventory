e55c7697736eea520983dd18de8dbde78ddbba69
Dmitrii Safronov <zimniy@cyberbrain.cc>
2026-04-29 20:19:48 +0400

    feat: enhance home view and URL structure
    - Updated HomeView to redirect authenticated users to "My items" page.
    - Modified URL patterns to reflect the new structure, changing inventory paths to start with "/my/".
    - Adjusted tests to align with the updated URL structure and ensure proper redirection and access control.
    
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.cc>

--- src/common/views.py
+++ src/common/views.py
@@ -1,5 +1,14 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
     template_name = "home.html"
+
+    def get(
+        self, request: HttpRequest, *args: object, **kwargs: object
+    ) -> HttpResponse:
+        if request.user.is_authenticated:
+            return redirect("inventory:my-items")
+        return super().get(request, *args, **kwargs)
