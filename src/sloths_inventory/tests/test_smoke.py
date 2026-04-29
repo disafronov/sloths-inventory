bdb577d6a8d156de57db3af450ebd7d9c3821c54
Dmitrii Safronov <zimniy@cyberbrain.cc>
2026-04-29 16:41:02 +0400

    test: remove smoke test for Django settings import
    - Deleted the `test_smoke.py` file, which contained a smoke test for importing the Django settings module. This removal helps streamline the test suite by eliminating redundant tests.
    
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.cc>

--- src/sloths_inventory/tests/test_smoke.py
+++
@@ -1,7 +0,0 @@
-def test_django_settings_import() -> None:
-    """Smoke test: Django settings module can be imported."""
-
-    # pytest-django configures Django based on DJANGO_SETTINGS_MODULE.
-    from django.conf import settings
-
-    assert settings is not None
