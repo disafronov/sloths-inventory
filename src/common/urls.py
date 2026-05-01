966c074ea207ca600812b1c67720d4696519636e
Dmitrii Safronov <zimniy@cyberbrain.cc>
2026-05-01 23:30:44 +0400

    feat: add user password change functionality
    - Introduced a new view for non-staff users to change their passwords, ensuring staff users are redirected to the Django admin for password management.
    - Updated the URL configuration to include a path for password change.
    - Created a new template for the password change page.
    - Added a button in the base template for users to access the password change page if they are not staff.
    
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.cc>

--- src/common/urls.py
+++ src/common/urls.py
@@ -2,6 +2,8 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 from django.views.i18n import set_language
 
+from .views import UserPasswordChangeView
+
 app_name = "common"
 
 urlpatterns = [
@@ -11,6 +13,11 @@ urlpatterns = [
             template_name="login.html", redirect_authenticated_user=True
         ),
         name="login",
+    ),
+    path(
+        "password/change/",
+        UserPasswordChangeView.as_view(),
+        name="password_change",
     ),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("i18n/setlang/", set_language, name="set_language"),
