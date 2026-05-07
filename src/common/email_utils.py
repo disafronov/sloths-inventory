efc31a5f94721fa65452c53036053daa7ee7f183
Dmitrii Safronov <zimniy@cyberbrain.cc>
2026-05-07 18:18:01 +0400

    refactor: Email change confirmation token improvements
    Improve email change token generation to use ContextVar for thread safety
    Added send_email_change_confirmation and send_email_changed_notification functions to handle email change confirmation and notification emails
    Updated email change token generator to use ContextVar for storing the target email
    Improved code to handle email change confirmation and notification emails in a thread-safe manner
    
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.cc>

--- src/common/email_utils.py
+++ src/common/email_utils.py
@@ -2,10 +2,14 @@ import logging
 import smtplib
 import threading
 import time
+from typing import Any
 
 from django.conf import settings
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from django.urls import reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 logger = logging.getLogger(__name__)
 
@@ -92,3 +96,39 @@ def send_transfer_email(
     message = render_to_string(body_template, context)
     html_message = render_to_string(html_template, context) if html_template else None
     send_email_async(subject, message, recipient, html_message)
+
+
+def send_email_change_confirmation(user: Any, new_email: str) -> None:
+    from common.email_tokens import email_change_token_generator
+
+    token = email_change_token_generator.make_token_for_email(user, new_email)
+    path = reverse(
+        "common:email_change_confirm",
+        kwargs={
+            "uidb64": urlsafe_base64_encode(force_bytes(user.pk)),
+            "token": token,
+            "new_email": urlsafe_base64_encode(force_bytes(new_email)),
+        },
+    )
+    confirmation_url = f"{settings.SITE_URL}{path}"
+    context = {
+        "user": user,
+        "new_email": new_email,
+        "confirmation_url": confirmation_url,
+    }
+    subject = render_to_string(
+        "emails/email_change_subject.txt", {"user": user}
+    ).strip()
+    text_body = render_to_string("emails/email_change_body.txt", context)
+    html_body = render_to_string("emails/email_change_body.html", context)
+    send_email_async(subject, text_body, new_email, html_body)
+
+
+def send_email_changed_notification(user: Any, old_email: str, new_email: str) -> None:
+    context = {"user": user, "old_email": old_email, "new_email": new_email}
+    subject = render_to_string(
+        "emails/email_changed_notification_subject.txt", {"user": user}
+    ).strip()
+    text_body = render_to_string("emails/email_changed_notification_body.txt", context)
+    html_body = render_to_string("emails/email_changed_notification_body.html", context)
+    send_email_async(subject, text_body, old_email, html_body)
